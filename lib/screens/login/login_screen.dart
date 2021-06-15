1b732dc116317a739fab05c3833e0b0272e40c96
Hafiz <hafizkn98@gmail.com>
2021-06-15 08:50:39 +0800

    Some configuration: Login - Body & Screen Student dashboard add http dependencies

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login_body.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-
 class LoginScreen extends StatefulWidget {
   static Route route() =>
       MaterialPageRoute(builder: (context) => LoginScreen());
@@ -12,15 +10,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  //final FirebaseAuth _auth = FirebaseAuth.instance;
-  //final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   String _password = '';
   String _username = 'TATATAA';
   String _errors = '';
   bool _showPassword = false;
 
-  // Getter and setter
   get password => _password;
   set password(value) => setState(() => _password = value);
 
@@ -32,14 +26,6 @@ class LoginScreenState extends State<LoginScreen> {
 
   get errors => _errors;
   set errors(value) => setState(() => _errors = value);
-
-  // checkAuthentic() async {
-  //   _auth.authStateChanges().listen((user) {
-  //     if (user != null) {
-  //       //print(user);
-  //     }
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {

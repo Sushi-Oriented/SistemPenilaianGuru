7ed5cf83a8fad6c73092e9ddc2558f8c8c20182f
Hafiz <hafizkn98@gmail.com>
2021-06-15 13:32:18 +0800

    Login button

--- lib/screens/login/login_body.dart
+++ lib/screens/login/login_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spg_test/constant.dart';
+import 'package:spg_test/screens/login/components/rounded_button.dart';
 import 'package:spg_test/screens/login/components/rounded_input_field.dart';
 import 'package:spg_test/screens/login/components/rounded_password_field.dart';
 import 'package:spg_test/screens/welcome/welcome_screen.dart';
@@ -12,7 +13,6 @@ class LoginBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Center(
       child: Container(
         child: Column(
@@ -30,40 +30,28 @@ class LoginBody extends StatelessWidget {
               ),
               RoundedInputField(
                 // Import from rounded_input_field.dart
-                hintText: "Insert your username",
+                hintText: "Username",
                 onChanged: (value) => _state.username = value,
               ),
               RoundedPasswordField(
                 // Import from rounded_passsword_field.dart
                 state: _state,
               ),
-              Container(
-                width: size.width * 0.5,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(29),
-                  child: ElevatedButton(
-                      onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => WelcomeScreen()),
-                          ),
-                      child: Text(
-                        'Log in',
-                        style: TextStyle(
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                        primary: spgPrimaryColor,
-                      )),
-                ),
+              RoundedLoginButton(
+                // Import from rounded_button.dart
+                color: spgPrimaryColor,
+                text: "Log In",
+                press: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WelcomeScreen()),
+                  )
+                },
               )
             ]),
       ),
     );
   }
-} //14:09
+}
+
+//14:09
